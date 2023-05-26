52faebb530ed78150cd007cbd65077b0aaf871cc
Nitin-Tomas31 <133589606+Nitin-Tomas31@users.noreply.github.com>
2023-05-26 23:21:31 +0530

    Update page-3.dart

--- lib/page-1/page-3.dart
+++ lib/page-1/page-3.dart
@@ -1,7 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/gestures.dart';
-import 'dart:ui';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:myapp/page-1/page-4.dart';
 import 'package:myapp/utils.dart';
 
@@ -29,8 +26,21 @@ class _NextPage extends State<NextPage> {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              const SizedBox(
-                height: 200,
+              Positioned(
+                // shapePUa (23:26)
+                left: 0 * fem,
+                top: 0 * fem,
+                child: Align(
+                  child: SizedBox(
+                    width: 304 * fem,
+                    height: 259 * fem,
+                    child: Image.asset(
+                      'assets/page-1/images/shape.png',
+                      width: 304 * fem,
+                      height: 259 * fem,
+                    ),
+                  ),
+                ),
               ),
               Container(
                 // undrawfilesearchingre3evy1koY (23:48)
@@ -59,6 +69,7 @@ class _NextPage extends State<NextPage> {
                   textAlign: TextAlign.center,
                   style: SafeGoogleFont(
                     'Inter',
+                    decoration: TextDecoration.none,
                     fontSize: 18 * ffem,
                     fontWeight: FontWeight.w600,
                     height: 1.2125 * ffem / fem,
@@ -78,6 +89,7 @@ class _NextPage extends State<NextPage> {
                   textAlign: TextAlign.center,
                   style: SafeGoogleFont(
                     'Inter',
+                    decoration: TextDecoration.none,
                     fontSize: 15 * ffem,
                     fontWeight: FontWeight.w300,
                     height: 1.2125 * ffem / fem,
@@ -90,7 +102,7 @@ class _NextPage extends State<NextPage> {
                 margin:
                     EdgeInsets.fromLTRB(115 * fem, 0 * fem, 115 * fem, 0 * fem),
                 width: double.infinity,
-                height: 42 * fem,
+                height: 34 * fem,
                 decoration: const BoxDecoration(
                   color: Color(0xff66cfde),
                 ),

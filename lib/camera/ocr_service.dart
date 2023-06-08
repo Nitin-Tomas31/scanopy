a3dd823391d4b97499e9f6d0818571eee8f8aff6
Nitin-Tomas31 <133589606+Nitin-Tomas31@users.noreply.github.com>
2023-06-08 09:59:58 +0530

    commit

---
+++ lib/camera/ocr_service.dart
@@ -0,0 +1,29 @@
+import 'dart:io';
+import 'package:google_ml_kit/google_ml_kit.dart';
+
+class OCRService {
+  Future<String> scanImage(String imagePath) async {
+    final inputImage = InputImage.fromFile(File(imagePath));
+    final textRecognizer = GoogleMlKit.vision.textRecognizer();
+    final RecognizedText recognisedText =
+        await textRecognizer.processImage(inputImage);
+
+    String result = "";
+    for (TextBlock block in recognisedText.blocks) {
+      for (TextLine line in block.lines) {
+        for (TextElement element in line.elements) {
+          result += element.text + " ";
+        }
+        result += "\n";
+      }
+      result += "\n";
+    }
+
+    textRecognizer.close();
+    return result;
+  }
+
+  void dispose() {
+    // Cleanup resources
+  }
+}

5f3ef6fd2e629d1e34d7d141306245024940b1d8
Nitin-Tomas31 <133589606+Nitin-Tomas31@users.noreply.github.com>
2023-06-19 22:27:51 +0530

    Update ocr_result_screen.dart

--- lib/camera/ocr_result_screen.dart
+++ lib/camera/ocr_result_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:translator/translator.dart';
 import 'ocr_service.dart';
 
 class OCRResultScreen extends StatefulWidget {
@@ -17,6 +18,7 @@ class OCRResultScreen extends StatefulWidget {
 
 class _OCRResultScreenState extends State<OCRResultScreen> {
   String? _scannedText;
+  String? _translatedText;
 
   @override
   void initState() {
@@ -28,7 +30,20 @@ class _OCRResultScreenState extends State<OCRResultScreen> {
     final scannedText = await widget.ocrService.scanImage(widget.imagePath);
     setState(() {
       _scannedText = scannedText;
+      _translateText(); // Call translation method after scanning
     });
+  }
+
+  Future<void> _translateText() async {
+    if (_scannedText != null) {
+      final translator =
+          GoogleTranslator(); // Create an instance of the translator
+      final translation =
+          await translator.translate(_scannedText!, from: 'en', to: 'ml');
+      setState(() {
+        _translatedText = translation.text;
+      });
+    }
   }
 
   @override
@@ -36,7 +51,7 @@ class _OCRResultScreenState extends State<OCRResultScreen> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: const Color.fromARGB(255, 68, 6, 238),
-        title: const Text('OCR Result'),
+        title: const Text('Translated Text'),
       ),
       body: SingleChildScrollView(
         child: Center(
@@ -46,8 +61,19 @@ class _OCRResultScreenState extends State<OCRResultScreen> {
               Padding(
                 padding: const EdgeInsets.all(20.0),
                 child: Text(
-                  _scannedText ?? '',
-                  style: const TextStyle(color: Colors.black),
+                  _translatedText ?? '',
+                  style: const TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold, // Set the font weight to bold
+                  ),
+                  maxLines: 1000, // Limit the text to 2 lines
+                  overflow: TextOverflow
+                      .ellipsis, // Display ellipsis if the text exceeds 2 lines
+                  textAlign: TextAlign.left, // Align the text to center
+                  softWrap: true, // Allow soft wrapping of the text
+                  textWidthBasis: TextWidthBasis
+                      .longestLine, // Set the width basis to the longest line
+                  textScaleFactor: 1.0, // Set the text scale factor to 1.0
                 ),
               ),
             ],
